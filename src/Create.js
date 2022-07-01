572c1972a1bdc17839842035dc2d8248b3e9d2e5
esmee953 <78962851+esmee953@users.noreply.github.com>
2022-07-02 01:01:01 +0200

    make post request

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,57 @@
-const Create  = () => {
+import { useState } from "react";
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('john');
+    const [body, setBody] = useState('');
+    const [isPending, setIsPending] = useState(false);
+ 
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        const blog = { title, author, body };
+        
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() =>{
+            console.log('Blog created');
+            setIsPending(false);
+        })
+    }
+
     return ( 
         <div>
             <h1>Create a new blog</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title</label>
+                <input 
+                    type="text" 
+                    required 
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                 />
+                <label>Blog content</label>
+                <textarea 
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>	
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="John">John</option>
+                    <option value="Jane">Jane</option>
+                </select>
+                {!isPending && <button>Add blog</button>}
+                {isPending && <button disabled>Adding...</button>}
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
      );
-}dV
+}
  
 export default Create;
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,13 @@
 import BlogList from './BlogList';
-import useFetch from './ufeFetch';
+import UseFetch from './UseFetch';
 
 const Home = () => {
-  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
+  const { data: blogs, isPending, error } = UseFetch("http://localhost:8000/blogs");
     return (
         <div className="background">
           { error && <div>{ error} </div>}
           { isPending && <div>loading...</div> }
-          {blogs && <BlogList blogs={blogs} title="all blogs"  /> }
+          { blogs && <BlogList blogs={blogs} title="all blogs"  /> }
         </div>
       );
 }
